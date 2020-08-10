30f3d29504d848ab669760cf48538366d0b1f70d
jiachongliu <jcl314159@gmail.com>
2020-08-11 01:32:59 +0800

    add 26 and 162

---
+++ LeetCode/26/26.cpp
@@ -0,0 +1,20 @@
+"""
+Date: 08/11/2020
+author: jiachongliu
+title: 26. Remove Duplicates from Sorted Array
+times: 1
+
+"""
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int j = 0;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (nums[i] != nums[j]) {
+				nums[++j] = nums[i];
+			}
+		}
+
+		return nums.empty() ? 0 : j + 1;
+	}
+}
