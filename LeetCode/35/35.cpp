6e74bc831ebc33b8f51336f5aac4b3337d2e90aa
jiachongliu <jcl314159@gmail.com>
2020-08-17 22:12:27 +0800

    add 35 and 643

---
+++ LeetCode/35/35.cpp
@@ -0,0 +1,26 @@
+/*
+Date: 08/17/2020
+Author: jiachongliu
+Title: 35.Search Insert Position
+Times: 1
+*/
+
+class Solution {
+public:
+	   int searchInsert(vector<int>& nums, int target) {
+	   	   int n = nums.size();
+
+	   	   if (n == 0) return 0;
+	   	   if (nums[n - 1] < target) return n;
+
+	   	   int left = 0, right = n - 1;
+	   	   while (left < right) {
+	   	   	   int mid = left + (right - left) / 2;
+
+	   	   	   if (nums[mid] < target) left = mid + 1;
+	   	   	   else right = mid;
+	   	   }
+
+	   	   return left;
+	   }
+}
---
+++ LeetCode/643/643.cpp
@@ -0,0 +1,26 @@
+/*
+Date: 08/17/2020
+Author: jiachongliu
+Title: 643. Maximum Arverage Subarray I
+Times; 1
+*/
+
+
+class Solution {
+public:
+	   double findMaxAverage(vector<int>& nums, int k) {
+	   	   int n = nums.size();
+
+	   	   vector<int> sums = nums;
+	   	   for (int i = 1; i < n; ++i) {
+	   	   	   sums[i] = sums[i - 1] + nums[i];
+	   	   }
+
+	   	   double mx = sums[k - 1];
+	   	   for (int i = k; i < n; ++i) {
+	   	   	   mx = max(mx, (double)sums[i] - sums[i - 1]);
+	   	   }
+
+	   	   return mx / 4;
+	   }
+}
