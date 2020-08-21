88cb502e46254796e00009d9ce17209ab2bf5d6a
jiachongliu <jcl314159@gmail.com>
2020-08-21 14:46:41 +0800

    add 1232 , 457 and 999

---
+++ LeetCode/1232/1232.cpp
@@ -0,0 +1,22 @@
+/*
+Date: 08/21/2020
+Author: jiachongliu
+Title: 1232. Check if it is a Straight Line
+Times: 1
+*/
+
+class Solution {
+public:
+	   book checkStraightLine(vector<vector<int>>& coordinates) {
+	       int x1 = coordinates[0][0], y1 = coordinates[0][1];
+	       int x2 = coordinates[1][0], y2 = coordinates[1][1];
+
+	       for (int i = 2; i < coordinates.size(); ++i) {
+	       	   int x3 = coordinates[i][0], y3 = coordinates[i][1];
+
+	       	   if ((x1 - x2) * (y1 - y3) != (x1 - x3) * (y1 - y2)) return false;
+	       }
+
+	       return true;
+	   }
+}
---
+++ LeetCode/457/457.cpp
@@ -0,0 +1,41 @@
+/*
+Date: 08/21/2020
+Author: jiachongliu	
+Title: 457. Circular Array Loop
+Times: 1
+*/
+
+class Solution {
+public:
+	   bool circularArrayLoop(vector<int>& nums) {
+	   	   int N = nums.size();
+
+	   	   for (int i = 0; i < N; ++i) {
+	   	   	   if (nums[i] == 0) continue;
+	   	   	   int dir = nums[i] > 0 ? 1 : -1;
+	   	   	   if (dfs(nums, i, N, dir)) return true;
+	   	   }
+
+	   	   return false;
+	   }
+
+	   bool dfs(vector<int>& nums, int i, int N, int dir) {
+	   	   if (nums[i] == 0) return false;
+	   	   if (nums[i] == INT_MAX) return true;
+
+	   	   int ndir = nums[i] > 0 ? 1 : -1;
+	   	   if (ndir == dir) {
+	   	       int j = (N + (nums[i] + i) % N) % N;
+
+	   	       nums[i] = INT_MAX;
+
+	   	       if (j != i && dfs(nums, j, N, dir)) return true;
+	   	       else {
+	   	           nums[i] = 0;
+	   	           return false;
+	   	       }
+	   	   }
+
+	   	   return false;
+	   }
+}
