566b4d2f15a3bddb82a17aef15774cdfef9bd29c
jiachongliu <jcl314159@gmail.com>
2020-09-03 21:23:21 +0800

    Create 300.cpp

---
+++ LeetCode/300/300.cpp
@@ -0,0 +1,27 @@
+/*
+Date: 09/03/2020
+Author: jiachongliu
+Title: 300. Longest Increasing Subsequence
+Times: 1
+*/
+
+class Solution {
+public:
+	   int lengthOfLIS(vector<int>& nums) {
+	   	   int n = nums.size();
+	   	   vector<int> dp(n, 1);
+
+	   	   int res = 0;
+	   	   for (int i = 0; i < n; ++i) {
+	   	   	   for (int j = 0; j < i; ++j) {
+	   	   	   	   if (nums[i] > nums[j]) {
+	   	   	   	   	   dp[i] = max(dp[i], dp[j] + 1);
+	   	   	   	   }
+	   	   	   }
+
+	   	   	   res = max(res, dp[i]);
+	   	   }
+
+	   	   return res;
+	   }
+}
