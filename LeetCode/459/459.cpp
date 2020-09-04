48397092ed71a590288be1ca1def3a3f8928b2e3
jiachongliu <jcl314159@gmail.com>
2020-09-04 21:57:59 +0800

    Create 459.cpp

---
+++ LeetCode/459/459.cpp
@@ -0,0 +1,13 @@
+/*
+Date: 09/04/2020
+Author: jiachongliu	
+Title: 459. Repeated Substring Pattern
+Times: 1
+*/
+
+class Solution {
+public:
+	   bool repeatedSubstringPattern(string s) {
+	   	   return (s + s).find(s, 1) != s.size();
+	   }
+}
