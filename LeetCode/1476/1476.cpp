5b9079e809eb99fe0265325c960bf48082c1434e
jiachongliu <jcl314159@gmail.com>
2020-09-06 12:35:12 +0800

    Update 1476.cpp

--- LeetCode/1476/1476.cpp
+++ LeetCode/1476/1476.cpp
@@ -14,8 +14,8 @@ public:
 	   }
 
 	   void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-	   	   for (int i = row1; i < row2; ++i) {
-	   	   	   for (int j = row2; j < row2; ++j) {
+	   	   for (int i = row1; i <=row2; ++i) {
+	   	   	   for (int j = row2; j <= row2; ++j) {
 	   	   	   	   rec[i][j] = newValue;
 	   	   	   }
 	   	   }
